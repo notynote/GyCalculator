f5740e1bb3f03682fd64b6d4dc7ea9f0790125b9
notynote <32334875+notynote@users.noreply.github.com>
2021-08-24 02:18:44 +0700

    basic ui

--- src/main/java/com/notynote/gycalculator/MainController.java
+++ src/main/java/com/notynote/gycalculator/MainController.java
@@ -1,4 +1,50 @@
 package com.notynote.gycalculator;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
 public class MainController {
+
+    @FXML
+    Text sumText;
+    @FXML
+    Text currentSum;
+    @FXML
+    Button zero;
+    @FXML
+    Button one;
+    @FXML
+    Button two;
+    @FXML
+    Button three;
+    @FXML
+    Button four;
+    @FXML
+    Button five;
+    @FXML
+    Button six;
+    @FXML
+    Button seven;
+    @FXML
+    Button eight;
+    @FXML
+    Button nine;
+    @FXML
+    Button clear;
+    @FXML
+    Button plus;
+    @FXML
+    Button minus;
+    @FXML
+    Button equal;
+    @FXML
+    Button plusOne;
+    @FXML
+    Button minusOne;
+    @FXML
+    Button plusTwo;
+    @FXML
+    Button minusTwo;
+
 }
