3086cecb741edaf3b5c3945b12aa140927e3e81b
notynote <32334875+notynote@users.noreply.github.com>
2021-08-24 05:42:31 +0700

    Version 1

--- src/main/java/com/notynote/gycalculator/GyCalculator.java
+++ src/main/java/com/notynote/gycalculator/GyCalculator.java
@@ -3,6 +3,7 @@ package com.notynote.gycalculator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,7 @@ public class GyCalculator extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(GyCalculator.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 405, 920);
         stage.setTitle("GyCalculator by NotyNote");
+        stage.getIcons().add(new Image(this.getClass().getResourceAsStream("GyCalculator.png")));
         stage.setScene(scene);
         stage.show();
     }
